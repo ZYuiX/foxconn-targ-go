d811eff95bbca3c56826eec537406d6049855fe0
ZYuiX <925312844@qq.com>
2024-07-24 21:40:11 +0800

    Second commit gin 路由参数

--- main.go
+++ main.go
@@ -10,19 +10,17 @@ import (
 使用gin框架完成一个简单的api，
 1.页面路由
 2.参数传递
-3.调用后能正确相应
+3.调用后能正确响应
 */
 
 func main() {
 	//启动一个默认路由
 	router := gin.Default() // 创建带有默认中间件的 Gin 路由器
-
-	router.GET("/ping", func(c *gin.Context) {
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "hello," + name + "!",
 		})
 	})
-
-	router.Run(":8080") // 监听并服务于 0.0.0.0:8080
-
+	router.Run(":8080") // 监听 0.0.0.0:8080
 }
